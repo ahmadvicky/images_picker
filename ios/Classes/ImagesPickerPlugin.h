62f58e52c88b244ba7aabbe38443d5fd702e54ee
vicky <vicky20and13@gmail.com>
2024-12-13 11:03:48 +0700

    update 1.0.7

--- ios/Classes/ImagesPickerPlugin.h
+++ ios/Classes/ImagesPickerPlugin.h
@@ -1,4 +1,4 @@
-//#import <Flutter/Flutter.h>
-//
-//@interface ImagesPickerPlugin : NSObject<FlutterPlugin>
-//@end
+#import <Flutter/Flutter.h>
+
+@interface ImagesPickerPlugin : NSObject<FlutterPlugin>
+@end
